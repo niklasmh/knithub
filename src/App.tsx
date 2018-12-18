c681d422010b212f1ff0baddc967355ce3495c74
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-12-18 16:44:33 +0100

    Make everything pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
-    return <div className="App">Cool</div>;
+    return <div className="App">Cool</div>
   }
 }
 
-export default App;
+export default App
