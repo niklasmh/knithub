8efb1a09211119145123aca22bc12f7d61a9749a
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-12-19 21:58:23 +0100

    Add components together to the main App component and fix grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class App extends Component {
+import Toolbar from './components/Toolbar'
+import Canvas from './components/Canvas'
+import Layers from './components/Layers'
+
+import { Grid } from './models/grid'
+
+interface IProps {}
+
+interface IState {
+  grid: Grid
+  scale: [number, number]
+}
+
+class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+
+    const grid: Grid = {
+      start: { x: 0, y: 0 },
+      width: 10,
+      height: 5
+    }
+
+    this.state = {
+      grid,
+      scale: [80, 80]
+    }
+  }
+
   render() {
-    return <div className="App">Cool</div>
+    const {
+      grid,
+      scale: [width, height]
+    } = this.state
+
+    return (
+      <div className="App">
+        <Toolbar />
+        <Layers />
+        <Canvas
+          width={width * grid.width}
+          height={height * grid.height}
+          grid={grid}
+        />
+      </div>
+    )
   }
 }
 
