4f9d36d05ef128aa650bb221562152926954b2fa
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-12-19 21:57:36 +0100

    Add layer and toolbar for mockup layout

---
+++ src/components/Layers.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Layers extends Component {
+  render() {
+    return <div>List of layers</div>
+  }
+}
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Toolbar extends Component {
+  render() {
+    return <div>Tools!!</div>
+  }
+}
